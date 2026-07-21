05723834417aa8b3823254cfa9aa308cd473efe2
Rohit00034 <rohitsahadev2506@gmail.com>
2026-07-21 20:44:03 +0530

    Course 100-101
    100. Introducing Enums
    4min
    Play
    101. Creating Dummy Data
    4min

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -9,10 +10,26 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Flutter Course',
+      amount: 19.99,
+      date: DateTime.now(), //the DateTime constructor is used here to pass an object to the main Expense() constructor .now() gives current date and time
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Cinema',
+      amount: 12.69,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+  ];
   @override
   Widget build(context) {
     return Scaffold(
-      body: Column(children: const [Text('The chart'), Text('The Expenses list..'),]),
+      body: Column(
+        children: const [Text('The chart'), Text('The Expenses list..')],
+      ),
     );
   }
 }
--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,14 +1,17 @@
 import 'package:uuid/uuid.dart';
 const uuid=Uuid();
+enum Category {food,travel,leisure,work} //this is an enum,unlike c++ these values are not contant integers ,an enum is a user defined datatype to make fixed set of allowed values 
 class Expense {
   Expense({
     required this.title,
     required this.amount,
     required this.date,
+    required this.category,
   }): id = uuid.v4(); //this : is an initializer list which is used to intialize properties which are not initialized by constructor arguments
   final String id;
   final String title;
   final double amount;
   final DateTime
   date; //DateTime here is a built-in data type in dart which allows us to use dates
+  final Category category; //an object of category
 }
