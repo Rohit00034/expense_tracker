25c87999ec991c64c2124caccf2cb23436b14bfe
Rohit00034 <rohitsahadev2506@gmail.com>
2026-07-22 20:41:02 +0530

    Course 102-103
    102. Efficiently Rendering Long Lists with ListView
    10min
    Play
    103. Using Lists Inside Of Lists
    2min

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
@@ -10,7 +11,7 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
-  final List<Expense> _registeredExpenses = [
+  final List<Expense> _registeredExpenses = [ //we cannot keep a list const as it is going to be modified in memory even if it is final as we will be adding expenses in the list  
     Expense(
       title: 'Flutter Course',
       amount: 19.99,
@@ -28,7 +29,10 @@ class _ExpensesState extends State<Expenses> {
   Widget build(context) {
     return Scaffold(
       body: Column(
-        children: const [Text('The chart'), Text('The Expenses list..')],
+        children: 
+        [
+        const Text('The chart'), 
+        Expanded(child: ExpensesList(expenses: _registeredExpenses))],//we wrapped ExpensesList with expanded() because THE Expenses() widget displays a column and the ExpensesList is also diplaying a list which is a column so when we build the app we do not see any ExpensesList before wrapping it with expanded .that is because there is a column inside a column and with flutter we run into such problem with such combination.     
       ),
     );
   }
---
+++ lib/expenses_list.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
+class ExpensesList extends StatelessWidget{
+  const ExpensesList({super.key,required this.expenses});
+  final List<Expense> expenses;
+  @override
+  Widget build(BuildContext context) {
+    /* return Column(); now here we needed to create a scrollable list of expenses widget ,so let's say a person adds about 1000 expenses but 
+    and we have to display all of them in a scrollable list,first thing we think of is a SingleChildScrollView wrapped over a column widget with
+    all the hundreds of expenses widgets inside the column.now the problem here is we cannot see all the hundreads of expenses at one time in
+    the phone screen ,and here whenever this column widget it created it also creates all the hundreds of expense widgets behing the scenes
+    which are not even visible on that screen viewport thus being cost heavy and utilizing resources unnecessarily and also here intially we 
+    do not know the lenght of how many widgets/expenses will be added so in such case we should use the ListView(children:[]) widget which 
+    still creates all the widgets immediately so we only get here is that it is automatically scrollable and creates a list of widgets just
+    like Column() .so here we wil use the follows */
+    return ListView.builder(itemCount: expenses.length,itemBuilder: (context,index)=>Text(expenses[index].title)); 
+    /*this builder constructor of ListView tell flutter to only build the widget when it is visible on the screen and takes a function as a 
+    parameter which should return a widget and have context/ctx and index as a parameter which are given by flutter itself.it also takes a 
+    itemscCount parameter which represents the count of the items in the list and the number of items in the list = the number of times the 
+    function will be called.Now above we have used an arrow function since we are returning the widget ,and here the index(dtype-int) parameter is used
+    to iterate through the list just like a for loop.So here the builder constructor of ListView improves the performance of the app by
+    buidling the widgets only when they are needed*/
+  }
+} 
